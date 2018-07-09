9960c9cd13ae9f9ddf1069118cf6ac29e1daf937
sgilissen <steve@gilissen.me>
2018-07-09 18:58:31 +0200

    Modify commandline options Add function to search for municipality and street Add conditional functions Add first search function (municipality)

--- limburgnetparser.py
+++ limburgnetparser.py
@@ -7,15 +7,19 @@ __maintainer__ = "Steve Gilissen"
 
 import urllib.request
 import json
+import pprint
 import sys
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("-s", "--street", dest="verbose", help="Find the street name. Specify the NIS code of the municipality to get best results.")
-parser.add_argument("-m", "--municipality", dest="verbose", help="Find the municipality.")
-parser.add_argument("-c", "--calendar", dest="verbose", help="Show the calendar.")
+parser.add_argument("-s", "--street", dest="street", action='store', help="Find the street name. Usage: STREET NIS")
+parser.add_argument("-m", "--municipality", dest="municipality", action='store', help="Find the municipality.")
+parser.add_argument("-c", "--calendar", dest="calendar", help="Show the calendar for ID.")
 args = parser.parse_args()
-parser.print_help()
+#parser.print_help()
+
+
+
 
 baseAPI = "https://api.limburg.net/public/"
 # API extensions:
@@ -28,14 +32,32 @@ calendarAPIExtension = "kalender/"
 
 
 def findMunicipality(municipality):
-    req = urllib.request.Request(baseAPI + searchAPIExtension)
-    print("Found the following municipalities:")
+    req = urllib.request.Request(baseAPI + searchAPIExtension + "gemeenten/search?query=" + municipality)
+    print("Found the following municipalities for " + municipality + ":")
+    response = urllib.request.urlopen(req)
+    data = response.read()
+    #print(data.decode('utf-8'))
+    parsed_data = json.loads(data)
+    pprint.pprint(parsed_data)
+
 
 
 def findStreet(street, nisCode):
     req = urllib.request.Request(baseAPI + searchAPIExtension)
     print("Found the following streets:")
+    print(street)
 
 def findEvents():
     req = urllib.request.Request(baseAPI + calendarAPIExtension)
     print("Found the following events:")
+
+
+if (args.street):
+    findStreet()
+elif (args.municipality):
+    #print(args.municipality)
+    findMunicipality(args.municipality)
+elif (args.calendar):
+    print("calendar")
+else:
+    parser.print_help()
